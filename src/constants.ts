6b24ef93f088f659251ca0f188af23adbfe994a8
Travis Reeder <treeder@gmail.com>
2020-07-20 12:49:26 -0700

    Add GOCHAIN

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,8 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  GOCHAIN = 60,
 }
 
 export enum TradeType {
--- src/entities/token.ts
+++ src/entities/token.ts
@@ -87,5 +87,6 @@ export const WETH = {
     'Wrapped Ether'
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether')
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.GOCHAIN]: new Token(ChainId.GOCHAIN, '0xcC237fa0A4B80bA47992d102352572Db7b96A6B5', 18, 'WGO', 'Wrapped GO'),
 }
