d40c140a45894e07f66c64447714ec1ffac37977
Reed Allman <reed@rdallman.com>
2020-09-02 15:21:56 -0700

    fix up tests for our factory address

--- test/pair.test.ts
+++ test/pair.test.ts
@@ -14,7 +14,7 @@ describe('Pair', () => {
 
   describe('#getAddress', () => {
     it('returns the correct address', () => {
-      expect(Pair.getAddress(USDC, DAI)).toEqual('0xAE461cA67B15dc8dc81CE7615e0320dA1A9aB8D5')
+      expect(Pair.getAddress(USDC, DAI)).toEqual('0x0c413f665D7bBAa5dFACd23C0C463ee97F5413B3')
     })
   })
 
